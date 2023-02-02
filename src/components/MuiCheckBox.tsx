1c279f4058ce92c0cd893e1e80a4964d71150a5c
Elmer Gallardo <elgallardo@gmail.com>
2023-02-02 14:41:48 -0600

    React Material UI Tutorial - 10 - Checkbox - Part 2

--- src/components/MuiCheckBox.tsx
+++ src/components/MuiCheckBox.tsx
@@ -1,20 +1,36 @@
-import { Box, FormControlLabel, Checkbox } from "@mui/material"
+import { Box, FormControlLabel, Checkbox, FormControl, FormLabel, FormGroup } from "@mui/material"
 import { useState } from "react"
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 
 export const MuiCheckBox = () => {
     const [acceptTnC, setAcceptTnC] = useState(false)
-    console.log({acceptTnC})
+    const [skills, setSkills] = useState<string[]>([])
+    console.log({skills})
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setAcceptTnC(event.target.checked)
+    }
+    const handleSkillChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        const index = skills.indexOf(event.target.value)
+        if (index === -1) {
+            setSkills([...skills, event.target.value])
+        }else{
+            setSkills(skills.filter((skill) => skill !== event.target.value))
+        }
     }
   return (
     <Box>
         <Box>
             <FormControlLabel 
                 label="I accept terms and conditions" 
-                control={<Checkbox checked={acceptTnC} onChange={handleChange}/>}
+                control={
+                    <Checkbox 
+                        size="small"
+                        color="secondary"
+                        checked={acceptTnC} 
+                        onChange={handleChange}
+                    />
+                }
             />
         </Box>
         <Box>
@@ -24,6 +40,26 @@ export const MuiCheckBox = () => {
                 checked={acceptTnC}
                 onChange={handleChange}
             ></Checkbox>
+        </Box>
+        <Box>
+            <FormControl>
+                <FormLabel>
+                    <FormGroup row>
+                    <FormControlLabel 
+                        label="HTML" 
+                        control={<Checkbox value="html" checked={skills.includes("html")} onChange={handleSkillChange}/>}
+                    />
+                    <FormControlLabel 
+                        label="CSS" 
+                        control={<Checkbox value="css" checked={skills.includes("css")} onChange={handleSkillChange}/>}
+                    />
+                    <FormControlLabel 
+                        label="JavaScript"
+                        control={<Checkbox value="javascript" checked={skills.includes("javascript")} onChange={handleSkillChange}/>}
+                    />
+                    </FormGroup>
+                </FormLabel>
+            </FormControl>
         </Box>
     </Box>
   )
