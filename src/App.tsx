ad2eba4d4c21ff6bace32f205f66b40da5ee8a87
Elmer Gallardo <elgallardo@gmail.com>
2023-02-02 14:03:59 -0600

    React Material UI Tutorial - 10 - Checkbox - Part 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 //import { MuiButton } from './components/MuiButton';
 //import { MuiTextField } from './components/MuiTextField';
 //import { MuiSelect } from './components/MuiSelect';
-import { MuiRadioButton } from './components/MuiRadioButton';
+//import { MuiRadioButton } from './components/MuiRadioButton';
+import { MuiCheckBox } from './components/MuiCheckBox';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/*<MuiButton></MuiButton> */}
       {/* <MuiTextField/> */}
       {/* <MuiSelect/> */}
-      <MuiRadioButton/>
+      {/* <MuiRadioButton/> */}
+      <MuiCheckBox/>
     </div>
   );
 }
