cef470bc56b6c83d0806b55bd0fd3852beab3593
Elmer Gallardo <elgallardo@gmail.com>
2023-02-02 10:28:48 -0600

    React Material UI Tutorial - 5 - Button Group

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,4 +1,4 @@
-import { Stack, Button, IconButton } from "@mui/material"
+import { Stack, Button, IconButton, ButtonGroup } from "@mui/material"
 import SendIcon from '@mui/icons-material/Send';
 
 export const MuiButton = () => {
@@ -28,6 +28,18 @@ export const MuiButton = () => {
             <IconButton aria-label="send" color="success" size="small"> {/* IconButton are more appropiate for navigation bars */}
                 <SendIcon/>
             </IconButton>
+        </Stack>
+        <Stack direction="row">
+            <ButtonGroup 
+                variant="contained" 
+                orientation="vertical" 
+                size="small" color="secondary" 
+                aria-label="alignment button group"
+            >
+                <Button onClick={() => alert("Left clicked")}>Left</Button>
+                <Button>Center</Button>
+                <Button>Right</Button>
+            </ButtonGroup>
         </Stack>
     </Stack>
   )
